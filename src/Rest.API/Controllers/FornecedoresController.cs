282ca505834ea4580203cf5d284cf755c0a09b3e
Matheus <matheustts8@gmail.com>
2021-08-22 19:32:07 -0300

    Implementando CRUD na controller de Fornecedores

--- src/Rest.API/Controllers/FornecedoresController.cs
+++ src/Rest.API/Controllers/FornecedoresController.cs
@@ -13,18 +13,86 @@ namespace Rest.API.Controllers
     public class FornecedoresController : MainController
     {
         private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IFornecedorService _fornecedorService;
         private readonly IMapper _mapper;
-        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper, IFornecedorService fornecedorService)
         {
             _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
+            _fornecedorService = fornecedorService;
         }
 
+        [HttpGet]
         public async Task<IEnumerable<FornecedorDto>> ObterTodos()
         {
             var fornecedores =   _mapper.Map<IEnumerable<FornecedorDto>>(await _fornecedorRepository.ObterTodos());
 
             return fornecedores;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorDto>> ObterPorId(Guid id)
+        {
+            var fornecedores = await ObterFornecedorProdutoEndereco(id);
+
+            if (fornecedores is null) return NotFound();
+
+            return fornecedores;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorDto>> Adicionar(FornecedorDto fornecedorDto)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorDto);
+
+            var result = await _fornecedorService.Adicionar(fornecedor);
+
+            if (!result) return BadRequest();
+
+            return Ok(fornecedor);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<FornecedorDto>> Atualizar(Guid id, FornecedorDto fornecedorDto)
+        {
+            if (id != fornecedorDto.Id) return BadRequest();
+
+            if (!ModelState.IsValid) return BadRequest();
+
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorDto);
+
+            var result = await _fornecedorService.Atualizar(fornecedor);
+
+            if (!result) return BadRequest();
+
+            return Ok(fornecedor);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorDto>> Excluir(Guid id)
+        {
+            var fornecedor = await ObterFornecedorEndereco(id);
+
+            if (fornecedor is null) return BadRequest();
+
+            var result = await _fornecedorService.Remover(id);
+
+            if (!result) return BadRequest();
+
+            return Ok(fornecedor);
+        }
+
+        public async Task<FornecedorDto> ObterFornecedorProdutoEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorDto>(await _fornecedorRepository.ObterFornecedorProdutosEndereco(id));
+        }
+
+        public async Task<FornecedorDto> ObterFornecedorEndereco(Guid id)
+        {
+            return _mapper.Map<FornecedorDto>(await _fornecedorRepository.ObterFornecedorEndereco(id));
         }
     }
 }
